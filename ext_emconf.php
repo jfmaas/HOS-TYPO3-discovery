75a67fdce35cb586301876ce53d3f116c5158a7d
Schleevoigt <rainer@schleevoigt.com>
2018-04-19 11:51:07 +0200

    README

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,22 +2,24 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Schaufenster',
-    'description' => 'An extension to manage a stock.',
+    'description' => 'A frontend  extension realising the HOS Schaufenster.',
     'category' => 'plugin',
-    'author' => 'John Doe',
-    'author_company' => 'John Doe Inc.',
-    'author_email' => 'john.doe@example.com',
+    'author' => 'Rainer Schleevoigt',
+    'author_company' => 'Staats- und Universitaetsbibliothek Hamburg Carl von Ossietzky',
+    'author_email' => 'rainer.schleevoigt@sub.uni-hamburg.de',
     'state' => 'alpha',
     'clearCacheOnLoad' => true,
-    'version' => '0.0.0',
+    'version' => '0.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-8.9.99',
+            'find' => '3.0.0',
+            'php' => '5.5.0-7.1.99'
         ]
     ],
     'autoload' => [
         'psr-4' => [
-            'MyVendor\\Schaufenster\\' => 'Classes'
+            'SubHH\\Schaufenster\\' => 'Classes'
         ]
     ],
 ];
